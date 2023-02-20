64adad5925933a3a234bcf42039e8ea47f112189
Eya-BenAmor <61431876+Eya-BenAmor@users.noreply.github.com>
2023-02-20 13:56:04 +0100

    Initial commit

---
+++ AM.UI.Console/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using System.Numerics;
+
+using AM.Core.Domain;
+//TP1. Question 7
+Console.WriteLine("Hello, World!");
+Plane plane= new Plane();
+plane.Capacity = 100;
+plane.ManufactureDate = new DateTime(2000,1,1);
+plane.MyPlaneType = PlaneType.BOING;
+
+//TP1. Question 8
+Plane plane2 = new Plane(PlaneType.BOING, 100, new DateTime(2000, 1, 1));
+
+//TP1 Question 9 (initialiseur d'objet)
+Plane plane3 = new Plane()
+{
+    Capacity= 100,
+    ManufactureDate= new DateTime(2000, 1, 1),
+    MyPlaneType= PlaneType.BOING
+};
+
+Passenger passenger = new Passenger();
+passenger.BirthDate = new DateTime(1990, 5, 1);
+//passenger.GetAge1();
+Console.WriteLine(passenger.Age);
+
+
+//tp1 question 12.b
+Passenger passanger2 =new Passenger();
+Passenger staff2= new Staff();
+Passenger traveller2 = new Traveller();
+Console.WriteLine(passanger2.GetPassengerType());
+Console.WriteLine(staff2.GetPassengerType());
+Console.WriteLine(traveller2.GetPassengerType());
+
+//tp1 question 13.c
+Passenger passenger3=new Passenger();
+//int calculatedAge = 5;
+//passenger3.GetAge(new DateTime(2000,1,1), ref calculatedAge);
+//Console.WriteLine(calculatedAge);
+passenger3.BirthDate = new DateTime(2000, 1, 1);
+//passenger3.GetAge(passenger3);
+Console.WriteLine(passenger3.Age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
